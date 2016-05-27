74e1719b9d195390931b00c1f0910136d7df78d1
Tyler Condon <condon.tyler@icloud.com>
2016-05-27 15:39:29 -0600

    Trying to send data.

--- src/main/kotlin/io/frontierrobotics/i2c/bus/Pi4jI2CBus.kt
+++ src/main/kotlin/io/frontierrobotics/i2c/bus/Pi4jI2CBus.kt
@@ -1,10 +1,16 @@
 package io.frontierrobotics.i2c.bus
 
+import com.pi4j.io.i2c.I2CFactory
 import org.springframework.stereotype.Service
 
 @Service
 class Pi4jI2CBus : I2CBus {
+
+    val bus = I2CFactory.getInstance(com.pi4j.io.i2c.I2CBus.BUS_1)
+    val dives= bus.getDevice(0x1A)
+
     override fun send(command: String) {
         println(command)
+        dives.write(0X02)
     }
 }
