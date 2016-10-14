c93cf42bc6ff30458c2a9afa9861dc13739dcb5c
Andy Condon <condon.andy@icloud.com>
2016-10-13 22:54:26 -0600

    This is easy!
    Now to figure out the right thing to do...

--- src/main/kotlin/io/frontierrobotics/i2c/api/Server.kt
+++ src/main/kotlin/io/frontierrobotics/i2c/api/Server.kt
@@ -13,6 +13,11 @@ class Server (val i2cController: I2CController)
         server.put("/bus/:bus/address/:address", i2cController.sendCommand)
         server.put("/bus/:bus/address/:address/internal_address/:internal_address", i2cController.sendCommand)
 
+        server.exception(IllegalArgumentException::class, {
+            response.setStatus(418, "My brew is not as strong as yours!")
+            response.send("Out of beans: ${exception.message}")
+        })
+
         server.start()
     }
 }
