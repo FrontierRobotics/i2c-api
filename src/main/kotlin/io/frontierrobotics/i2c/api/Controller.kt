18213a21a96b232f7be4df06d9947ccb8c312505
Andy Condon <condon.andy@icloud.com>
2017-01-24 10:53:09 -0700

    Different (better) way of returning data.

--- src/main/kotlin/io/frontierrobotics/i2c/api/Controller.kt
+++ src/main/kotlin/io/frontierrobotics/i2c/api/Controller.kt
@@ -3,6 +3,7 @@ package io.frontierrobotics.i2c.api
 import io.frontierrobotics.i2c.I2CBus
 import io.frontierrobotics.i2c.I2CData
 import io.frontierrobotics.i2c.I2CDevice
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -30,25 +31,25 @@ class Controller(val i2CBus: I2CBus)
     }
 
     @GetMapping("/bus/{bus}/address/{address}")
-    @ResponseBody
     fun receiveCommandFromAddress(@PathVariable bus: Int,
                                   @PathVariable address: Int,
-                                  @RequestParam(required = false) size: Int?) : I2CData
+                                  @RequestParam(required = false) size: Int?) : ResponseEntity<I2CData>
     {
         val device = I2CDevice(address)
+        val data = i2CBus.receive(device, size)
 
-        return i2CBus.receive(device, size)
+        return ResponseEntity.ok(data)
     }
 
     @GetMapping("/bus/{bus}/address/{address}/internal_address/{internalAddress}")
-    @ResponseBody
     fun receiveCommandFromInternalAddress(@PathVariable bus: Int,
                                           @PathVariable address: Int,
                                           @PathVariable internalAddress: Int,
-                                          @RequestParam(required = false) size: Int?) : I2CData
+                                          @RequestParam(required = false) size: Int?) : ResponseEntity<I2CData>
     {
         val device = I2CDevice(address, internalAddress)
+        val data = i2CBus.receive(device, size)
 
-        return i2CBus.receive(device, size)
+        return ResponseEntity.ok(data)
     }
 }
